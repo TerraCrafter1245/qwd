fa4a97db5b3ebeb2fd6f91e40e895bed25095978
TerraCrafter1245 <70241247+TerraCrafter1245@users.noreply.github.com>
2021-11-03 10:24:08 -0500

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/* eslint-env node */
+
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = (_env, _argv) => {
+    const mode = process.env.NODE_ENV || "production";
+    console.log(`Building ${mode}...`);
+
+    return {
+        mode,
+        entry: "./js/ruffle.js",
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "ruffle.js",
+            publicPath: "",
+            chunkFilename: "core.ruffle.[contenthash].js",
+            clean: true,
+        },
+        devtool: "source-map",
+        plugins: [
+            new CopyPlugin({
+                patterns: [{ from: "LICENSE*" }, { from: "README.md" }],
+            }),
+        ],
+    };
+};
